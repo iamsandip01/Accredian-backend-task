e980714df0efb3503efd3777c755cf07d5125006
Sandip Patra <imailsandip@gmail.com>
2024-07-22 03:20:35 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const nodemailer = require('nodemailer');
-const dotenv = require('dotenv').config;
+const dotenv = require('dotenv');
 
 dotenv.config()
 const cors = require('cors');
